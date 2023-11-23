bd8619c9ade4e9511da71d42682eb98f6e8f688e
Justin0929m <scholtz0929@gmail.com>
2023-11-23 15:55:17 +0200

    90% complete

--- code.js
+++ code.js
@@ -41,12 +41,12 @@ const searchInput = async ()=>{
 }
 
 const displayCountries = (countries)=>{
-    let displayData = document.querySelector('#test')
+    let displayData = document.querySelector('#countries')
 
     displayData.innerHTML = ''
     countries.forEach((countries) =>{
         displayData.innerHTML += `
-            <div class="card">
+            <div class="card" onclick='fetchCountry("${countries.name.common}")'>
                     <img src="${countries.flags.png}" id="flag-img" class="card-img"  width="250" alt="">
                     <div class="card-body">
                         <h1 class="card-title" id="country-title">${countries.name.common}</h1>
@@ -73,20 +73,63 @@ function dropdownFunc(){
 
 // Dark Mode / Light Mode
 
-const changeThemeDark = ()=>{
-    let body = document.querySelector("body")
-    let darkMode = document.querySelector("#dark-mode"); 
+let buttonClicked = false
+
+let darkMode = document.querySelector("#dark-mode");
+let lightMode = document.querySelector("#light-mode");
+
+darkMode.addEventListener('click', ()=>{
+    buttonClicked = !buttonClicked
     
-    body.classList.add('dark-bg')
-    darkMode.classList.add("display-mode-theme");
-}
+    if(buttonClicked){
+        document.documentElement.setAttribute('data-theme', 'dark')
+        lightMode.setAttribute('data-display', 'show')
+        darkMode.setAttribute('data-display', 'none')
+    } else{
+        document.documentElement.setAttribute('data-theme', 'light')
+        lightMode.setAttribute('data-display', 'none')
+        darkMode.setAttribute('data-display', 'show')
+    }
+})
 
-const changeThemeLight = ()=>{
-    let body = document.querySelector("body");
-    let lightMode = document.querySelector("#light-mode");
-
-    body.classList.remove("dark-bg");
-    lightMode.classList.add("display-mode-theme");
-}
+lightMode.addEventListener('click', ()=>{
+    buttonClicked = !buttonClicked
+    
+    if(buttonClicked){
+        document.documentElement.setAttribute('data-theme', 'dark')
+        darkMode.setAttribute('data-display', 'none')
+        lightMode.setAttribute('data-display', 'show')
+    } else{
+        document.documentElement.setAttribute('data-theme', 'light')
+        darkMode.setAttribute('data-display', 'show')
+        lightMode.setAttribute('data-display', 'none')
+    }
+})
 
 fetchCountries()
+
+// Single Country
+
+const fetchCountry = async (country) =>{
+    let res = await fetch(`https://restcountries.com/v3.1/name/${country}`)
+    let data = await res.json() 
+    
+    localStorage.setItem('country', JSON.stringify(data))
+    
+}
+
+const displayCountry = ()=>{
+    let countryArr = localStorage.getItem('country') ? localStorage.getItem('country') : []
+
+    let displaySection = document.querySelector('#country-name')
+
+    displaySection.innerHTML = ''
+
+    countryArr.forEach((country) =>{
+        displaySection.innerHTML += `
+            <h1>${country.name.common}</h1>
+        `
+    })
+}
+
+displayCountry()
